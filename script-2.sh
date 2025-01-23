5ae8b3643f713b4c11befc2fa23f9da3317213b5
siva-manohar <132001142+siva-manohar@users.noreply.github.com>
2025-01-23 16:25:42 +0530

    Create script-2.sh

---
+++ script-2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+n=10
+
+echo "Even Numbers - "
+i=1
+
+# -le means less than or equal to
+while [ $i -le $n ]   
+do
+# arithmetic operations is performed with 'expr'   
+rs=`expr $i % 2`
+
+if [ $rs == 0 ]
+then
+echo "  $i"
+  
+# end of if loop
+fi
+
+# incrementing i by one
+((++i))
+  
+# end of while loop  
+done
+
+# Now printing odd numbers
+echo "Odd Numbers - "
+i=1
+  
+while [ $i -le $n ]
+do
+  
+rs=`expr $i % 2`
+
+if [ $rs != 0 ]
+then
+
+echo "  $i"
+  
+fi
+
+((++i))
+done
